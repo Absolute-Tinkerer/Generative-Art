69c2455f6303b9556ab76b1a84cdc53cfa1ef775
Absolute-Tinkerer <62893707+Absolute-Tinkerer@users.noreply.github.com>
2021-12-05 09:41:58 -0500

    Update examples.py
    Fixed a couple bugs; added expanding circle bodies and Perlin rounding.

--- examples.py
+++ examples.py
@@ -14,7 +14,6 @@ from PIL import Image
 from PyQt5.QtGui import QColor, QPen, QPixmap
 from PyQt5.QtCore import QPointF, QRect
 
-# Absolute Tinkerer imports
 import painter
 from utils import QColor_HSV, save, Perlin2D
 
@@ -120,9 +119,6 @@ def draw_vectors(nx, ny, width, height, seed=random.randint(0, 100000000), flow_
     # Save the vector image
     save(p, fname=v_path, folder='.')
 
-    # End this painter, but don't exit
-    p.endProgram(exit_code=1):
-
     # Now draw the flow field. Start by initializing a new painter
     p = painter.Painter(width, height)
     p.setRenderHint(p.Antialiasing)  # allow smooth drawing
@@ -161,7 +157,11 @@ def draw_vectors(nx, ny, width, height, seed=random.randint(0, 100000000), flow_
     save(p, fname=f_path, folder='.')
 
 
-def draw_flow_field(width, height):
+def draw_flow_field(width, height, seed=random.randint(0, 100000000)):
+    # Set the random seed for repeatability
+    np.random.seed(seed)
+
+    # These are color hues
     colors = [200, 140, 70, 340, 280]
     for i, mod in enumerate(colors):
         print('Starting Image %s/%s' % (i + 1, len(colors)))
@@ -201,7 +201,7 @@ def draw_flow_field(width, height):
                     p.setPen(QPen(QColor_HSV(hue, sat, 255, 20), 2))
 
                     # angle between -pi and pi
-                    angle = 2 * p_noise[int(x_s), int(y_s)] * math.pi
+                    angle = p_noise[int(x_s), int(y_s)] * math.pi
 
                     # Compute the new point
                     x_f = x_s + STEP_SIZE * math.cos(angle)
@@ -220,4 +220,179 @@ def draw_flow_field(width, height):
                     else:
                         x_s, y_s = x_f, y_f
 
-            save(p, fname=f'image_{mod}_{num}', folder='.', overwrite=True)
+            save(p, fname=f'image_{mod}_{num}_{seed}', folder='.', overwrite=True)
+
+
+def draw_perlin_rounding(width, height, fname, seed=random.randint(0, 100000000)):
+    # Ensure we don't overwrite paths
+    assert not os.path.exists(fname), 'Image already exists!'
+
+    # Set the random seed for repeatability
+    np.random.seed(seed)
+
+    # Initialize a new painter
+    p = painter.Painter(width, height)
+    p.setRenderHint(p.Antialiasing)
+
+    # Draw the background color
+    p.fillRect(0, 0, width, height, QColor(0, 0, 0))
+
+    # Set the pen color
+    p.setPen(QColor(200, 200, 200))
+
+    print('Creating Noise...', end='', flush=True)
+    noise = Perlin2D(width, height, 1, 1)
+    print('Done!')
+
+    # The maximum line length and step size
+    MAX_LENGTH = 1000
+    STEP_SIZE = 0.001 * max(width, height)
+
+    # Compute a grid 200x200 points, centered in the screen
+    dx, dy = width / (200 + 1), height / (200 + 1)
+    POINTS = [[(i+1)*dx, (j+1)*dy] for i in range(200) for j in range(200)]
+
+    for i, (x_s, y_s) in enumerate(POINTS):
+        print(f'{100 * (i + 1) / len(POINTS):.1f}'.rjust(5) + '% Complete', end='\r')
+
+        # The current line length tracking variable
+        c_len = 0
+        while c_len < MAX_LENGTH:
+            # angle between -pi and pi
+            angle = math.pi*noise[int(x_s), int(y_s)]
+
+            # Round the angle to pi/4 increments
+            angle = round(angle / (math.pi / 4)) * (math.pi / 4)
+
+            # Compute the new point
+            x_f = x_s + STEP_SIZE * math.cos(angle)
+            y_f = y_s + STEP_SIZE * math.sin(angle)
+
+            # Draw the line
+            p.drawLine(x_s, y_s, x_f, y_f)
+
+            # Update the line length
+            c_len += math.sqrt((x_f - x_s) ** 2 + (y_f - y_s) ** 2)
+
+            # Break from the loop if the new point is outside our image bounds
+            # or if we've exceeded the line length; otherwise update the point
+            if (x_f < 0 or x_f >= width or y_f < 0 or y_f >= height or
+                    c_len > MAX_LENGTH):
+                break
+            else:
+                x_s, y_s = x_f, y_f
+
+    print('100% Complete!')
+    save(p, fname=f'{fname}_{seed}', folder='.')
+
+
+class Body:
+    def __init__(self, x, y, vx, vy):
+        self._position = np.array([x, y], dtype=np.float64)
+        self._velocity = np.array([vx, vy], dtype=np.float64)
+
+    @property
+    def position(self):
+        return self._position
+
+    @property
+    def velocity(self):
+        return self._velocity
+
+    def update(self, dt):
+        # update the body position
+        self._position = self._position + dt*self._velocity
+
+
+class ExpandingCircleRandom:
+    def __init__(self, radius, num_bodies, center=(0, 0), v_limits=(-2, 2)):
+        self._bodies = [Body(center[0] + radius*math.cos(i*2*math.pi/num_bodies),
+                             center[1] + radius*math.sin(i*2*math.pi/num_bodies),
+                             v_limits[0]+(v_limits[1]-v_limits[0])*random.random(),
+                             v_limits[0]+(v_limits[1]-v_limits[0])*random.random()) for i in range(num_bodies)]
+
+    def draw(self, dt, painter):
+        # Connect the dots between each body
+        for i in range(len(self._bodies)):
+            # Handle the wrapping case
+            if i == len(self._bodies) - 1:
+                p1 = QPointF(*self._bodies[i].position)
+                p2 = QPointF(*self._bodies[0].position)
+            else:
+                p1 = QPointF(*self._bodies[i].position)
+                p2 = QPointF(*self._bodies[i+1].position)
+            painter.drawLine(p1, p2)
+
+        # Update the position of each body
+        for i in range(len(self._bodies)):
+            self._bodies[i].update(dt)
+
+
+class ExpandingCircleNoise:
+    def __init__(self, radius, num_bodies, noise, center=(0, 0), v_max=2):
+        self._bodies = [Body(center[0] + radius*math.cos(i*2*math.pi/num_bodies),
+                             center[1] + radius*math.sin(i*2*math.pi/num_bodies),
+                             0, 0) for i in range(num_bodies)]
+        self._v_max = v_max
+        self._noise = noise
+
+    def draw(self, dt, painter):
+        # Connect the dots between each body
+        for i in range(len(self._bodies)):
+            # Handle the wrapping case
+            if i == len(self._bodies) - 1:
+                p1 = QPointF(*self._bodies[i].position)
+                p2 = QPointF(*self._bodies[0].position)
+            else:
+                p1 = QPointF(*self._bodies[i].position)
+                p2 = QPointF(*self._bodies[i + 1].position)
+            painter.drawLine(p1, p2)
+
+            # Try to update the velocity for each body. If we can't its because the point is beyond the noise
+            # field we've created, so at that point, just maintain velocity.
+            try:
+                a = math.pi*self._noise[int(p1.x()), int(p1.y())]
+                v = np.array([self._v_max*math.cos(a), self._v_max*math.sin(a)])
+                self._bodies[i]._velocity = v
+            except IndexError:
+                pass
+
+        # Update the position of each body
+        for i in range(len(self._bodies)):
+            self._bodies[i].update(dt)
+
+
+def draw_delta_body(width, height, seed=random.randint(0, 100000000), mode='noise'):
+    assert mode in ['noise', 'random'], 'Mode must either be "noise" or "random"'
+
+    # Set the random seed for repeatability
+    np.random.seed(seed)
+    random.seed(seed)
+
+    # Initialize the painter
+    p = painter.Painter(width, height)
+    p.setRenderHint(p.Antialiasing)  # Allow smooth drawing
+
+    # Draw the background color
+    p.fillRect(0, 0, width, height, QColor(0, 0, 0))
+
+    # Set the pen color
+    p.setPen(QPen(QColor(220, 220, 220, 5), 1))
+
+    # Initialize the expanding circle centered in the canvas
+    if mode == 'random':
+        circle = ExpandingCircleRandom(width/8, 100, center=(width/2, height/2), v_limits=(-2, 2))
+    elif mode == 'noise':
+        noise = Perlin2D(width, height, 5, 5)
+        circle = ExpandingCircleNoise(width/6, 200, noise, center=(width/4, height/2), v_max=5)
+    else:
+        circle = None
+
+    # Initialize the delta time we're applying to each update
+    dt = 0.3
+
+    iterations = 1000
+    for i in range(iterations):
+        circle.draw(dt, p)
+
+    save(p, fname=f'delta_{mode}_{seed}', folder='.', overwrite=True)
